8d7414f30693902087fd040166ec64c94e3fab04
masum691 <masumhu691@gmail.com>
2021-08-29 20:41:35 +0600

    add display single meal

--- js/app.js
+++ js/app.js
@@ -19,11 +19,12 @@ const updateSearchMeal = meals => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div onclick="displayMeal(${meal.idMeal})" class="card">
-        <img class="p-2" src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div onclick="displayMeal(${meal.idMeal})" class="card  border-0 shadow-lg">
+        <img class="p-3" src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${meal.strMeal}</h5>
           <p class="card-text">${meal.strInstructions.slice(0,200)}</p>
+          <a href="${meal.strYoutube}" class="btn btn-primary" target="_blank">Watch</a>
         </div>
         
       </div>
@@ -37,5 +38,20 @@ const displayMeal = mealId => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data.meals[0]))
+    .then(data => displaySingleMeal(data.meals[0]))
+}
+
+const displaySingleMeal = (meal) => {
+    const displayMeal = document.getElementById('display-meal');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img class="p-3" src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${meal.strMeal}</h5>
+      <p class="card-text">${meal.strInstructions}</p>
+      <a href="${meal.strYoutube}" class="btn btn-primary" target="_blank">Watch This Meal</a>
+    </div>
+    `
+    displayMeal.appendChild(div);
 }
